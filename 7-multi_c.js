892498ee833f4b6a84b5b1b0a1d49f539e8659d7
Anasbhr1 <anasbhr1@hotmail.com>
2025-07-13 22:33:10 +0100

    multi c

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+if (isNaN(num)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < num; i++) {
+        console.log("C is fun");
+    }
+}
